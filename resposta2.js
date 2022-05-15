6753bba2da0588174330dfefc4fc893d99359f17
Aluno de Front End Senai <AlunoDeFrontEndSenai@gmail.com>
2022-05-15 18:20:33 -0300

    Inicializando projeto

---
+++ resposta2.js
@@ -0,0 +1,17 @@
+
+for (let alunos = 0; alunos <= 20; alunos ++){
+
+
+if (alunos == 0){
+    console.log("zero " + alunos);
+} else{ // avalia se é par ou impar
+    
+    if (alunos % 2 == 0){  // numero par
+        console.log("par " + alunos);
+        }
+        else{
+            console.log("impar " + alunos);
+        }
+}
+}
+
